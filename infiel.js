1baf907e6bd7bfb4941795f1534e30fd151e783b
natyyp <natypiresoliveira@gmail.com>
2023-05-11 17:13:07 -0300

    fodase

---
+++ infiel.js
@@ -0,0 +1,82 @@
+// const numero1 = Number(prompt("digite um numero!"))
+// const number1 = Number(prompt("digite outro numero!"))
+
+// if(numero1 > number1){
+//     console.log(`${numero1} é maior que ${number1}`);
+// }else if(number1 > numero1){
+//     console.log(`${number1} é maior que ${numero1}`);
+// }else if(numero1 === number1) çlI^2{
+//     console.log(`${number1} é igual ao ${numero1}`);
+// }
+
+
+// let paisOrigem
+// switch (paisOrigem) {
+//     case "Brasil":
+//         console.log("Brasileiro");
+//         break;
+//     case "Estados Unidos":
+//     console.log("Americano");
+//     break
+//     case "Italia":
+//         console.log("Italiano");
+//         break
+
+// default:
+// console.log("Nacionalidade não encontrada");        
+// }
+
+
+// const pokemon = prompt("digite o nome de um pokemon inicial!")
+
+// switch(pokemon.toLowerCase()){
+//     case "Bulbassauro".toLowerCase():
+//     console.log("você escolheu o Bulbassauro!");
+//     break;
+//     case "Charmander".toLowerCase():
+//     console.log("você escolheu o Charmander!");
+//     break;
+//     case "squirtle".toLowerCase():
+//     console.log("você esccolheu o squirtle")
+//     default:
+//     console.log("tem que escolher os iniciais seu cabaço");            
+//     break;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// const caralho = prompt("você concluiu o ensino médio?")
+// const desgraca = Number(prompt("qual sua idade?"))
+// const fodase = prompt("você cursa, ou vai cursar faculdade? digite o nome dela?")
+
+
+
+// function matricularusario(caralho,desgraça,fodase){
+//     if(caralho === "sim" && desgraca >= 18 && fodase === "não") {
+ 
+//     console.log("matricula concluida");
+ 
+//  } else if(desgraca < 18)
+ 
+//   console.log("não tem como entrar na faculdade sendo de menor!"); {
+     
+//   }if(fodase !== "sim")
+
+//   console.log("voce não pode se matricular pois não concluiu o ensino médio"); {
+
+//   }
+
+
+ 
+//  }
+ 
+ 
+ 
